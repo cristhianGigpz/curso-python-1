cb8cabc77ee93406f00d9a28591b2db55487eab9
cristhianGigpz <cjacevedot@gmail.com>
2025-11-05 09:24:37 -0500

    clase 2 variables y tipos

---
+++ variables-tipos.py
@@ -0,0 +1,46 @@
+dato = "Hola"
+print(dato)
+
+dato = 10
+print(dato)
+
+x,y = 5,7
+print(x)
+print(y)
+
+# 2guia = "invalid"  # Invalid: starts with a digit
+# mi-variable = "invalid"  # Invalid: contains a hyphen
+# variable! = "invalid"  # Invalid: contains an exclamation mark
+# mi variable = "invalid"  # Invalid: contains a space
+
+cadena1 = "Hola"
+cadena2 = 'Mundo'
+cadena3 = """Esto es una 
+cadena Multilínea"""
+
+print(cadena1, cadena2)
+print(cadena1 + "-" + cadena2)
+print(cadena3)
+
+
+numeroEntero = 10
+numeroDecimal = 3.14
+valorBooleano = True
+
+print(numeroEntero)
+print(numeroDecimal)
+print(valorBooleano)
+
+
+#variables explicitas
+edad: int = 25
+altura: float = 1.75
+nombre: str = "Juan"
+es_estudiante: bool = True
+print(edad)
+print(altura)
+print(nombre)
+print(es_estudiante)
+
+edad = 36
+print(edad)
