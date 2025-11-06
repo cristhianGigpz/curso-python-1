84442a94c7a80b503cf99b01a7c4a3c614814d66
cristhianGigpz <cjacevedot@gmail.com>
2025-11-06 09:56:07 -0500

    clase 5 tipos bool y none

---
+++ booleano.py
@@ -0,0 +1,30 @@
+v = True
+f = False
+
+print(type(v))
+print(type(f))
+
+print("Valor de v:", v)
+print("Valor de f:", f)
+
+print(bool(1))
+print(bool(0))
+print(bool(-5))
+print(bool(""))
+print(bool("Hola"))
+print(bool([]))
+print(bool([1, 2, 3]))
+
+print(1 > 2)
+print(3 <= 3)
+
+x = 10
+print(isinstance(v, bool))
+print(isinstance(x, int))
+
+# tipo NONE
+y = None
+print(type(y))
+
+print(type(""))
+print(type(False))
