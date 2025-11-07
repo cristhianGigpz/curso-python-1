a33545b440bd00099b1bbb76044facb4c4f9d0f5
cristhianGigpz <cjacevedot@gmail.com>
2025-11-07 10:28:28 -0500

    clase 6 operadores aritmeticos y asignacion

---
+++ operadores-aritmeticos-asignacion.py
@@ -0,0 +1,40 @@
+x = 2
+y = 5
+
+# Suma
+suma = x + y
+print("Suma:", suma)
+# Resta
+resta = x - y
+print("Resta:", resta)
+# Multiplicación
+multiplicacion = x * y
+print("Multiplicación:", multiplicacion)
+# División
+division = x / y
+print("División:", division)
+# División entera
+division_entera = x // y
+print("División entera:", division_entera)
+# Módulo
+modulo = x % y
+print("Módulo:", modulo)
+# Potenciación
+potenciacion = x ** y
+print("Potenciación:", potenciacion)
+
+print("el valor inicial de x es:", x)
+print("el valor inicial de y es:", y)
+
+y = 10
+x = 1
+x += 1  # Equivalente a x = x + 1
+print("el nuevo valor de x es:", x)
+
+x += y
+print("el nuevo valor de x es:", x)
+
+x *= y
+print("el nuevo valor de x es:", x)
+
+print(z:= x + y)  # Asignación con expresión (Python 3.8+)
